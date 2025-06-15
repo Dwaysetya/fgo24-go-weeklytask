2220c22e6085fb0927615046fd5743e540349d43
dwisetbu@gmail.com <dwisetbu@gmail.com>
2025-06-15 20:16:47 +0700

    feat: Update code more dynamically separates items in each menu drinks for easy handling

--- models/minuman.go
+++ models/minuman.go
@@ -1,5 +1,6 @@
 package models
 
+
 type Minuman struct{
 	Name string
 	Harga int64
@@ -10,4 +11,16 @@ func (d Minuman) GetHarga()int64 {return d.Harga}
 
 func NewMinuman (name string, harga int64)Minuman{
 	return Minuman{Name: name, Harga: harga}
+}
+
+var daftarMinuman = []Item{
+	NewMinuman("Es Teh Manis", 5000),
+	NewMinuman("Es Jeruk", 6000),
+	NewMinuman("Kopi Hitam", 7000),
+	NewMinuman("Kopi Susu", 8000),
+	NewMinuman("Air Mineral", 4000),
+}
+
+func GetAllMinuman() []Item {
+	return daftarMinuman
 }
